53d831646273ebbc1f4ba130f4fe70380665ae5e
rhys117 <rhys117@gmail.com>
2017-08-15 19:56:38 +1000

    only show priorities above 'x' added

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,7 +1,10 @@
 $(document).ready(function () {
   set_checkbox_from_storage('show_past_reminders');
+  set_checkbox_from_storage('above_priority');
   hide_reminders_if_checked('show_past_reminders', 'past_date');
   hide_reminders_if_checked('show_future_reminders', 'future_date');
+  set_select_from_storage('show_above_priority');
+  hide_reminders_below_priority();
 });
 
 function hide_reminders_if_checked(button, affected_class) {
@@ -20,14 +23,26 @@ function set_checkbox_from_storage(button) {
   }
 }
 
+function hide_reminders_below_priority() {
+  var priority_level = $('.show_above_priority').find(':selected').text();
+  localStorage.setItem('show_above_priority', priority_level);
 
+  for (var i = 5; i > 0; i--) {
+    console.log('.priority_' + i)
+    $('.priority_' + i).show();
+  }
+
+  for (var i = priority_level; i >= 0; i--) {
+    $('.priority_' + (i - 1)).hide();
+  }
+}
 
 function add_to_notes(note) {
   $('#notes').val($('#notes').val() + note + ' ')
 }
 
-function confirm_action(message) {
-  return confirm(message);
+function set_select_from_storage(value) {
+  $('.' + value).val(localStorage.getItem(value));
 }
 
 // $(function () {
