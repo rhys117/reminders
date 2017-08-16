1c17061bb016d8daa08859d83a0af0a8a11e6674
rhys117 <rhys117@gmail.com>
2017-08-16 23:36:12 +1000

    added edit function, refactored some functions

--- reminders.rb
+++ reminders.rb
@@ -6,8 +6,6 @@ require "bcrypt"
 require "date"
 require "pry"
 
-config_file File.expand_path('../config.yml', __FILE__)
-
 def data_path
   if ENV["RACK_ENV"] == 'test'
     File.expand_path("../test/data", __FILE__)
@@ -96,17 +94,41 @@ def error_message(params)
   false
 end
 
-def generate_new_reminder_hash(params, reminders_hash, reminder_date, service_type)
-  { id: next_element_id(reminders_hash[reminder_date]),
-     reference: params[:reference].strip.to_i,
-     vocus_ticket: params[:vocus_ticket].strip.to_i,
-     service_type: service_type,
-     priority: params[:priority],
-     notes: params[:notes].strip,
-     complete: false }
+def generate_reminder(params, reminders_hash, date)
+  service_type = set_service_type(params)
+  new_reminder = { id: next_element_id(reminders_hash[date]),
+                   reference: params[:reference].strip.to_i,
+                   vocus_ticket: params[:vocus_ticket].strip.to_i,
+                   service_type: service_type,
+                   priority: params[:priority],
+                   notes: params[:notes].strip,
+                   complete: false }
+end
+
+def make_past_uncomplete_current
+  reminders_hash = load_reminders_list
+  out_of_date = []
+  today = Date.today
+
+  reminders_hash.each do |date, reminders|
+    if date < today
+      reminders.reject! { |reminder| out_of_date << reminder unless reminder[:complete] }
+    end
+  end
+
+  reminders_hash.reject! { |_, reminders| reminders == nil || reminders.empty? }
+  reminders_hash[today] = reminders_hash[today] || []
+
+  out_of_date.each do |reminder|
+    reminder[:id] = next_element_id(reminders_hash[today])
+    reminders_hash[today] << reminder
+  end
+
+  reminders_hash
 end
 
 configure do
+  config_file File.expand_path('../config.yml', __FILE__)
   enable :sessions
   set :session_secret, "0165359735"
 end
@@ -191,30 +213,26 @@ post "/sign_out" do
 end
 
 post "/add_reminder" do
-  reminders_hash = load_reminders_list
-  reminder_date = calculate_date(params[:date], params[:custom_date])
-
   error = error_message(params)
+
   if error
     session[:error] = error
 
     @reminder_list = load_reminders_list
     erb :reminder_list
   else
-    service_type = set_service_type(params)
-    new_reminder = generate_new_reminder_hash(params, reminders_hash, reminder_date, service_type)
+    reminders_hash = load_reminders_list
+    date = calculate_date(params[:date], params[:custom_date])
+    new_reminder = generate_reminder(params, reminders_hash, date)
 
-    if reminders_hash[reminder_date] == nil
-      reminders_hash[reminder_date] = [new_reminder]
-    else
-      reminders_hash[reminder_date] << new_reminder
-    end
+    reminders_hash[date] = reminders_hash[date] || []
+    reminders_hash[date] << new_reminder
 
     File.open(reminder_path, "w") do |file|
       file.write reminders_hash.to_yaml
     end
 
-    session[:success] = "#{service_type.upcase} #{params[:notes]} <br/> #{display_date(reminder_date)}"
+    session[:success] = "#{set_service_type(params).upcase} #{params[:notes]} <br/> #{display_date(date)}"
     redirect "/"
   end
 end
@@ -269,6 +287,50 @@ post "/:date/:id/delete" do
   redirect "/"
 end
 
+get "/:date/:id/edit" do
+  id = params[:id].to_i
+  @date = Date.parse(params[:date])
+
+  reminders_hash = load_reminders_list
+
+  reminder_index = reminders_hash[@date].index { |reminder| reminder[:id] == id }
+  @reminder = reminders_hash[@date][reminder_index]
+
+  @is_vocus = @reminder[:service_type].include?('VOCUS')
+  @service_type = @reminder[:service_type].gsub(' (VOCUS)', '')
+
+  erb :edit
+end
+
+post "/:date/:id/edit" do
+  error = error_message(params)
+
+  if error
+    session[:error] = error
+
+    redirect("/#{params[:old_date]}/#{params[:old_id]}/edit")
+  else
+    reminders_hash = load_reminders_list
+    old_date = Date.parse(params[:old_date])
+    old_index = reminders_hash[old_date].index { |reminder| reminder[:id] == params[:old_id].to_i }
+
+    new_date = calculate_date(params[:new_date], params[:custom_date])
+    new_reminder = generate_reminder(params, reminders_hash, new_date)
+
+    reminders_hash[old_date].delete_at(old_index)
+    reminders_hash[new_date] = reminders_hash[new_date] || []
+    reminders_hash[new_date] << new_reminder
+    reminders_hash.reject! { |_, reminders_array| reminders_array.empty? }
+
+    File.open(reminder_path, "w") do |file|
+      file.write reminders_hash.to_yaml
+    end
+
+    session[:success] = session[:success] = "#{set_service_type(params).upcase} #{params[:notes]} <br/> #{display_date(new_date)}"
+    redirect "/"
+  end
+end
+
 post "/delete_all_complete" do
   reminders_hash = load_reminders_list
 
@@ -285,37 +347,13 @@ post "/delete_all_complete" do
   redirect "/"
 end
 
-# super messy must be refactored
 post "/make_incomplete_current" do
-  reminders_hash = load_reminders_list
-
-  today = Date.today
-
-  out_of_date = []
-  reminders_hash.each do |date, reminders|
-    if date < today
-      reminders.each do |reminder|
-        unless reminder[:complete]
-          out_of_date << reminder
-          reminder_index = reminders_hash[date].index { |re| reminder[:id] == re[:id] }
-          reminders_hash[date].delete_at(reminder_index)
-        end
-      end
-    end
-  end
-
-  reminders_hash[today] = [] if reminders_hash[today] == nil
-
-  out_of_date.each do |reminder|
-    reminder[:id] = next_element_id(reminders_hash[today])
-    reminders_hash[today] << reminder
-  end
-
-  reminders_hash.reject! { |_, reminders_array| reminders_array.empty? }
+  reminders_hash = make_past_uncomplete_current
 
   File.open(reminder_path, "w") do |file|
     file.write reminders_hash.to_yaml
   end
+
   session[:success] = "All past incomplete reminders moved to today"
   redirect "/"
 end
