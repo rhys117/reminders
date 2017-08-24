88f0b7d45768a0e8257282c10578996c2bae58b2
rhys117 <rhys117@gmail.com>
2017-08-24 23:11:17 +1000

    stripped for example case

--- new_user.rb
+++ new_user.rb
@@ -9,16 +9,22 @@ password = gets.chomp
 
 hashed_password = BCrypt::Password.create(password)
 
-reminder_path = File.expand_path("../data/#{user}/reminder_list.yml", __FILE__)
+root_path = File.expand_path('../', __FILE__)
 
-dirname = File.dirname(reminder_path)
-unless File.directory?(dirname)
-  FileUtils.mkdir_p(dirname)
+user_data_path = "./data/#{user}"
+
+if File.directory?(user_data_path)
+  puts "Error: user already exists. Please delete from '/data' before proceeding"
+else
+  FileUtils.mkdir_p(user_data_path)
+  FileUtils.mkdir_p(user_data_path + '/custom_clips')
+  File.open(user_data_path + '/reminder_list.yml', 'w') { |file| file.write('{}') }
+  creds_path = File.expand_path("../users.yml", __FILE__)
+
+  users = YAML.load_file(creds_path, '/users.yml')
+  users["#{user}"] = hashed_password.to_s
+
+  File.open(creds_path, "w") do |file|
+    file.write users.to_yaml
+  end
 end
-
-
-File.open(reminder_path, "w") do |file|
-  file.write reminders_hash.to_yaml
-end
-
-File.open
